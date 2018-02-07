fa050696825a89ae515c60448e63021fe98067c6
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2018-02-07 16:37:21 +0100

    Menu: opening and closing

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
+import { FabContainer, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'menu',
   templateUrl: 'menu.html'
 })
 export class MenuComponent {
+  @ViewChild('fablist') fablist: FabContainer;
+  open = false;
 
-  text: string;
+  constructor(public navCtrl: NavController,
+    private eRef: ElementRef) { }
 
-  constructor() {
-    console.log('Hello MenuComponent Component');
-    this.text = 'Hello World';
+/*   @HostListener('document:click', ['$event'])
+  public documentClick(event: Event): void {
+    console.log('ok');
+    if(open) { this.fablist.close();} else {this.fablist.open();}
+  } */
+
+  reduceMenu() {
+    this.fablist.close();
+    this.open= false;
+  }
+
+  gotoPage(page) {
+    this.navCtrl.push(page);
   }
 
 }
+
