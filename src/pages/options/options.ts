29855ac739693e22790c3a6a759d886a2e16515c
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2018-02-28 20:57:04 +0100

    Option page: basic page

--- src/pages/options/options.ts
+++ src/pages/options/options.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the OptionsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 @IonicPage()
 @Component({
@@ -15,11 +10,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OptionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public afAuth: AngularFireAuth, public authService: AuthServiceProvider, private storage: Storage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OptionsPage');
   }
+
+  logout() {
+    this.afAuth.auth.signOut().then(() => {
+      this.storage.setItem('uid', '');
+      this.navCtrl.setRoot('LoginPage');
+    });
+  };
 
 }
